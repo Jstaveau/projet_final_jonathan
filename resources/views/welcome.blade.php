168fc4460180677d10966bfd98a79059cf6f8b22
Jonathan Staveau <jonathanstaveau@MacBook-Pro-de-Jonathan-2.local>
2022-05-13 12:07:12 +0200

    creation branche jonathan + laravelisation page home ok

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+@section('content')
+    @include('partials.home.carouselSection')
+    @include('partials.home.productArea')
+    @include('partials.home.blogArea')
+    @include('partials.home.subscribeArea')
+@endsection
